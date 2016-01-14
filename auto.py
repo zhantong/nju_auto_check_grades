a38d81b877414f71bb6945cef41f152deb388f82
zhantong1994 <zhantong1994@163.com>
2016-01-14 11:09:18 +0800

    修改为面向对象，修改代码降低耦合

--- auto.py
+++ auto.py
@@ -17,74 +17,17 @@ logging.basicConfig(level=logging.DEBUG,
 def image_to_string(img):
 	res=subprocess.Popen(['tesseract',img,'stdout','-l eng', '-psm 6'],shell=False,stdout=subprocess.PIPE).communicate()[0]  # 生成同名txt文件
 	return res.decode().strip().replace(' ','')
-def get_verify_code(opener,url):
+def deal_with_image(file_path,file_name):
 	threshold=(100,100,100)
-	with opener.open(url) as f:
-		imgBuf=BytesIO(f.read())
-		img=Image.open(imgBuf)
-		pix=img.load()
-		for y in range(img.size[1]):
-			for x in range(img.size[0]):
-				if pix[x,y]>threshold:
-					pix[x,y]=(255,255,255,255)
-				else:
-					pix[x,y]=(0,0,0,255)
-	img.save('temp.png')
-	text=image_to_string('temp.png')
-	return text
-def get_verify_code_auto_try(opener,url):
-	try_count=10
-	code_length=4
-	while try_count:
-		code=get_verify_code(opener,url)
-		logging.debug('verify code:%s'%code)
-		if len(code)==code_length:
-			logging.info('verify code success (cannot make sure)')
-			return code
-		try_count-=1
-		logging.debug('verify code failed')
-	return -1
-def login(opener):
-	regular=re.compile(r'<input\s*type="hidden"\s*name="(.*?)"\s*value="(.*?)"\s*/?>')
-	with opener.open('http://pyb.nju.edu.cn/') as f:
-		content=f.read().decode()
-		login_url=re.findall(r'<form id="login_form".*?action="(.*?)"\s*>',content)[0]
-		login_form=regular.findall(content)
-		login_form=dict(login_form)
-	url='%s?%s'%(login_url,urlencode(login_form))
-	with opener.open(url) as f:
-		con=f.read().decode()
-		verify_code_url=re.findall(r'<img id="vcodeimg" src="(.*?)"/>',con)[0]
-		verify_code_url=urljoin(login_url,verify_code_url)
-		r=regular.findall(con)
-		r=dict(r)
-	code=get_verify_code_auto_try(opener,verify_code_url)
-	r['IDToken1'],r['IDToken2']=private.get_account_pwd()
-	r['inputCode']=code
-	with opener.open(login_url,data=urlencode(r).encode()) as f:
-		content=f.read().decode()
-		if content.find('验证码错误')==-1:
-			return 0
-		return -1
-def login_auto_try(opener):
-	try_count=10
-	while try_count:
-		if login(opener)==0:
-			logging.info('login success')
-			return 0
-		try_count-=1
-		logging.debug('login failed')
-	return -1
-def get_score(opener):
-	reg_tr=re.compile(r'<tr.*?>\s*(.*?)\s*</tr>',re.S)
-	reg_td=re.compile(r'<td.*?>\s*(.*?)\s*</td>',re.S)
-	score_table=[]
-	with opener.open('http://pyb.nju.edu.cn/student/queryScoreInfo.action') as f:
-		con=f.read().decode()
-		r=reg_tr.findall(con)
-		for row in r:
-			score_table.append(reg_td.findall(row))
-	return score_table
+	img=Image.open(file_path)
+	pix=img.load()
+	for y in range(img.size[1]):
+		for x in range(img.size[0]):
+			if pix[x,y]>threshold:
+				pix[x,y]=(255,255,255,255)
+			else:
+				pix[x,y]=(0,0,0,255)
+	img.save(file_name)
 def send_mail(score_table):
 	text=''
 	for row in score_table:
@@ -102,11 +45,80 @@ def send_mail(score_table):
 	s.login(private.get_email_account(),private.get_email_password())
 	s.send_message(msg)
 	s.quit()
+class Test:
+	def __init__(self):
+		cj=http.cookiejar.CookieJar()
+		self.opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+		self.url='http://pyb.nju.edu.cn/'
+	def get_verify_code(self,url):
+		with self.opener.open(url) as f:
+			imgBuf=BytesIO(f.read())
+		deal_with_image(imgBuf,'temp.png')
+		text=image_to_string('temp.png')
+		return text
+	def get_verify_code_auto_try(self,url):
+		try_count=10
+		code_length=4
+		while try_count:
+			code=self.get_verify_code(url)
+			logging.debug('verify code:%s'%code)
+			if len(code)==code_length:
+				logging.info('verify code success (cannot make sure)')
+				return code
+			try_count-=1
+			logging.debug('verify code failed')
+		return -1
+	def login(self):
+		regular=re.compile(r'<input\s*type="hidden"\s*name="(.*?)"\s*value="(.*?)"\s*/?>')
+		with self.opener.open(self.url) as f:
+			content=f.read().decode()
+			login_url=re.findall(r'<form id="login_form".*?action="(.*?)"\s*>',content)[0]
+			login_form=regular.findall(content)
+			login_form=dict(login_form)
+		url='%s?%s'%(login_url,urlencode(login_form))
+		with self.opener.open(url) as f:
+			con=f.read().decode()
+			verify_code_url=re.findall(r'<img id="vcodeimg" src="(.*?)"/>',con)[0]
+			verify_code_url=urljoin(login_url,verify_code_url)
+			r=regular.findall(con)
+			r=dict(r)
+		code=self.get_verify_code_auto_try(verify_code_url)
+		r['IDToken1'],r['IDToken2']=private.get_account_pwd()
+		r['inputCode']=code
+		with self.opener.open(login_url,data=urlencode(r).encode()) as f:
+			content=f.read().decode()
+			if content.find('验证码错误')==-1:
+				self.get_query_urls(content)
+				return 0
+			return -1
+	def get_query_urls(self,content):
+		base_url=re.findall(r'<base href="(.*?)">',content)[0]
+		score_url=re.findall(r'<a href="(.*?)" title="成绩查看">',content)[0]
+		score_url=urljoin(base_url,score_url)
+		self.score_url=score_url
+	def login_auto_try(self):
+		try_count=10
+		while try_count:
+			if self.login()==0:
+				logging.info('login success')
+				return 0
+			try_count-=1
+			logging.debug('login failed')
+		return -1
+	def get_score(self):
+		reg_tr=re.compile(r'<tr.*?>\s*(.*?)\s*</tr>',re.S)
+		reg_td=re.compile(r'<td.*?>\s*(.*?)\s*</td>',re.S)
+		score_table=[]
+		with self.opener.open(self.score_url) as f:
+			con=f.read().decode()
+			r=reg_tr.findall(con)
+			for row in r:
+				score_table.append(reg_td.findall(row))
+		return score_table
 if __name__=='__main__':
-	cj=http.cookiejar.CookieJar()
-	opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-	login_auto_try(opener)
-	score_table=get_score(opener)
+	t=Test()
+	t.login_auto_try()
+	score_table=t.get_score()
 	logging.info(score_table)
-#	if len(score_table)!=2:
-#		send_mail(score_table)
+	if len(score_table)!=2:
+		send_mail(score_table)
